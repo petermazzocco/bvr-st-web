8943e11aa8af299a0d57d933db1e04557619b2a7
Peter Mazzocco <petermazzocco@gmail.com>
2025-07-21 13:21:02 -0700

    added otp

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,17 +4,13 @@ import {
   getAuthTokenServer,
   getUserIdFromTokenServer,
 } from "@/server/user/actions";
-import {
-  getCustomerPaymentMethods,
-  getPaymentHistory,
-  getSubscriptionPaymentInfo,
-} from "@/server/stripe/actions";
+import { getCustomerPaymentMethods } from "@/server/stripe/actions";
 import { AccountAddressCard } from "@/components/cards/account-address-card";
 import { AccountHeaderCard } from "@/components/cards/account-header-card";
 import { AccountOrdersCard } from "@/components/cards/account-orders-card";
-import { AccountPaymentHistoryCard } from "@/components/cards/account-payment-history-card";
 import { AccountInfoCard } from "@/components/cards/account-info-card";
 import { AccountPaymentMethodsCard } from "@/components/cards/account-payment-methods-card";
+import { OTPCard } from "@/components/cards/otp-card";
 
 export default async function ProfilePage() {
   const userId = await getUserIdFromTokenServer();
@@ -28,8 +24,6 @@ export default async function ProfilePage() {
   const user = await getUserDetails(authToken, userId);
   const orders = await getUserOrders(authToken, userId, 1, 10);
   const paymentMethods = await getCustomerPaymentMethods(userId, authToken);
-  const subscriptionInfo = await getSubscriptionPaymentInfo(userId, authToken);
-  const paymentHistory = await getPaymentHistory(userId, authToken, "10");
 
   if (!user || (user && !user.success)) {
     return <div>No user found</div>;
@@ -38,7 +32,7 @@ export default async function ProfilePage() {
   return (
     <div className="min-h-screen">
       <AccountHeaderCard user={user?.data} />
-
+      {!user.data?.emailVerified && <OTPCard />}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
         <div className="space-y-2">
           <div className="flex flex-col md:flex-row gap-4">
