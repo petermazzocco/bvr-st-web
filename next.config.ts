add45f388291b827ca405e0ca16fc6a3221a9f7e
Peter Mazzocco <petermazzocco@gmail.com>
2025-08-02 12:36:58 -0700

    chore:brought back orders card + added ppr config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    ppr: true,
+  },
   images: {
     loader: "custom",
     loaderFile: "./lib/imageLoader.ts",
