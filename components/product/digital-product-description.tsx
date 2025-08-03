ca060956148cd7307625d1648eee698b1ff18ab1
Peter Mazzocco <petermazzocco@gmail.com>
2025-08-03 09:47:56 -0700

    added digital product component

--- components/product/digital-product-description.tsx
+++ components/product/digital-product-description.tsx
@@ -4,14 +4,11 @@ import { Product } from "@/lib/shopify/types";
 import { AddToCartButton } from "@/components/cart/add-to-cart-button";
 import { Separator } from "../ui/separator";
 import Link from "next/link";
-import SizeChartModal from "../modals/size-chart-modal";
-import Image from "next/image";
 import { AdditionalDetails, Affiliate } from "@/lib/types";
-import { CareInstructionsModal } from "../modals/care-instructions-modal";
 import { AffiliateSelection } from "../utils/affiliate-selection";
 import { cookies } from "next/headers";
 
-export async function ProductDescription({
+export async function DigitalProductDescription({
   product,
   isMember,
   details,
@@ -31,17 +28,6 @@ export async function ProductDescription({
   );
   const earnedPoints = Math.floor(basePoints);
 
-  const totalStock = product.variants.reduce((total, variant) => {
-    return total + (variant.availableForSale ? 1 : 0);
-  }, 0);
-
-  const stockStatus =
-    totalStock === 0
-      ? "Out of Stock"
-      : totalStock === product.variants.length
-        ? "In Stock"
-        : `${totalStock}/${product.variants.length} variants available`;
-
   return (
     <>
       <div className="mb-2 flex flex-row items-center justify-between">
@@ -59,30 +45,13 @@ export async function ProductDescription({
       <div>
         <p className="text-xs font-muted-foreground">{product.description}</p>
       </div>
-      <div className="mb-2 flex flex-row text-[0.6rem] items-center text-muted-foreground justify-start md:justify-end gap-1">
-        Make 4 interest free payments every 2 weeks with{" "}
-        <Image
-          src={
-            "https://logos-world.net/wp-content/uploads/2024/10/Shop-Pay-Logo.png"
-          }
-          alt="Shop PAY"
-          width={50}
-          height={50}
-          className="mt-0.5"
-        />
-        <Link href="/" className="underline">
-          Learn More
-        </Link>
-      </div>
       <VariantSelector options={product.options} variants={product.variants} />
-
       <div className="mb-6 text-xs leading-tight text-muted-foreground flex flex-row items-center justify-between">
         {details?.additionalSpecs?.map((spec, index) => (
           <div key={index} className="flex flex-row items-center">
             <span>{spec.value}</span>
           </div>
         ))}
-        <SizeChartModal />
       </div>
       <Separator className="my-4" />
       <div className="flex flex-col gap-4">
@@ -118,28 +87,7 @@ export async function ProductDescription({
             </>
           )}
         </p>
-        <p
-          className={`${totalStock === 0 ? "text-red-600" : totalStock === product.variants.length ? "text-green-600" : "text-yellow-600"}`}
-        >
-          {stockStatus}
-        </p>
       </div>
-      <div className="pt-2 flex flex-row items-center justify-between mb-4">
-        <Link
-          className="text-xs text-muted-foreground cursor-pointer underline   "
-          href="/legal/return-policy"
-        >
-          Return Policy
-        </Link>
-        <CareInstructionsModal details={details} />
-        <Link
-          className="text-xs text-muted-foreground cursor-pointer underline   "
-          href="/contact"
-        >
-          Request Size
-        </Link>
-      </div>
-      <div className="flex justify-start items-center"></div>
     </>
   );
 }
