19bcdd6de8bf612ad4d9d6d1708afcda6f1d93ba
Peter Mazzocco <petermazzocco@gmail.com>
2025-08-07 16:55:04 -0700

    change width

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -4,11 +4,11 @@ import { SignInCard } from "@/components/cards/signin-card";
 export default function Page() {
   return (
     <div className="flex min-h-screen w-full pt-16">
-      <div className="hidden md:flex flex-col items-center justify-center w-1/2 py-2">
+      <div className="hidden md:flex flex-col items-center justify-center w-1/3 py-2">
         <SignInCard />
       </div>
 
-      <div className="hidden md:block w-1/2 relative">
+      <div className="hidden md:block w-2/3 relative">
         <Image
           src="/auth.jpg"
           alt="Authentication illustration"
