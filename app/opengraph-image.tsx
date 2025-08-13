9542c0f5d823e54fcb84005ac298aada1545ecc2
Peter Mazzocco <petermazzocco@gmail.com>
2025-08-13 06:53:46 -0700

    hotfix: opengraph image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -3,5 +3,5 @@ import OpengraphImage from "@/components/utils/opengraph-image";
 export const runtime = "edge";
 
 export default async function Image() {
-  return await OpengraphImage();
+  return await OpengraphImage({ imgSrc: "/logo.png", title: "BVR ST CO" });
 }
