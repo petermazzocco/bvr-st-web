eb1d0ccef8785a44a4a2d4b0424f709cafcfa8ca
Peter Mazzocco <petermazzocco@gmail.com>
2025-08-21 07:19:11 -0700

    removed opengraph-image from parent rooy

--- app/opengraph-image.tsx
+++
@@ -1,7 +0,0 @@
-import OpengraphImage from "@/components/utils/opengraph-image";
-
-export const runtime = "edge";
-
-export default async function Image() {
-  return await OpengraphImage({ imgSrc: "/logo.png", title: "BVR ST CO" });
-}
