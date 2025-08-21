8e15b6487ab1485ef14d79747bc92c8dc52983b1
Peter Mazzocco <petermazzocco@gmail.com>
2025-08-21 07:14:00 -0700

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Navbar } from "@/components/layout/navbar";
-import { Inter, Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/layout/footer";
 import { Toaster } from "sonner";
@@ -16,12 +16,11 @@ import {
   getAuthTokenServer,
   getUserIdFromTokenServer,
 } from "@/server/user/actions";
-import PageTransitionContext from "@/contexts/page-transition-context";
 
 export const metadata: Metadata = {
   title: "BVR ST CO",
   description:
-    "Discover unique streetwear and products for Oregon State fans that support the Oregon State University student-athletes.",
+    "Engineering the future of college athletics through Oregon State.",
   keywords: [
     "oregon state beavers",
     "dam nation collective",
@@ -55,14 +54,14 @@ export const metadata: Metadata = {
   openGraph: {
     title: "BVR ST CO",
     description:
-      "The new way to support Oregon State University student-athletes.",
+      "Engineering the future of college athletics through Oregon State.",
     url: "https://bvrstco.com",
     siteName: "BVR ST CO",
     locale: "en_US",
     type: "website",
     images: [
       {
-        url: "/logo.png",
+        url: "https://bvrstco.com/logo.png",
         width: 1200,
         height: 630,
         alt: "BVR ST CO",
@@ -73,10 +72,10 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "BVR ST CO",
     description:
-      "The new way to support Oregon State University student-athletes.",
+      "Engineering the future of college athletics through Oregon State.",
     creator: "@bvrstco",
     site: "@bvrstco",
-    images: ["/logo.png"],
+    images: ["https://bvrstco.com/logo.png"],
   },
   metadataBase: new URL("https://bvrstco.com"),
   alternates: {
