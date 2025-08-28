dc2f810bded63b92853e775fd8a188a1dd901c42
Peter Mazzocco <petermazzocco@gmail.com>
2025-08-27 19:05:21 -0700

    changed durating

--- contexts/smooth-scrolling.tsx
+++ contexts/smooth-scrolling.tsx
@@ -4,7 +4,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.6, smoothWheel: true }}>
       {children}
     </ReactLenis>
   );
