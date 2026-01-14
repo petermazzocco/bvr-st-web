737bd0eee72ea45c5a927a1af5ed7426183d055d
Peter Mazzocco <petermazzocco@gmail.com>
2026-01-13 18:16:46 -0800

    udpated robots + sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
+  : "https://bvrstco.com";
 
 export default function robots() {
   return {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,12 +7,11 @@ type Route = {
 
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
+  : "https://bvrstco.com";
 
 export const dynamic = "force-dynamic";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-
   const routesMap = [""].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString(),
